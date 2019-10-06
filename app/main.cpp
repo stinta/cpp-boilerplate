e8636f0230739e0dc301cf84dcfec1b2fcb847a8
stinta <stinta@gmail.com>
2019-10-05 23:30:16 -0400

    fixes after vlagrind using smart pointer and remove unused code

--- app/main.cpp
+++ app/main.cpp
@@ -5,10 +5,13 @@ int main()
 {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator;
-    if( terminator )
-    {
-        std::cout << "DONE" << std::endl;
-    }
-    return 0;
+    //Not sure the intend of these lines are; not condition where terminator is
+    // set to a value was defined.  A logic can be added for for now since not useful
+    // in the code it is being removed
+    /* bool terminator;
+     if( terminator )
+     {
+       std::cout << "DONE" << std::endl;
+     }*/
+     return 0;
 }
